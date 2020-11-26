073cfddd3889d957f6d329636371de2c7b53702e
ChivaSdev <melvinlucchini357@gmail.com>
2020-11-26 11:56:04 +0100

    Ajout des tests de NegaMax sur Nim et TicTacToe

--- src/plays/TestOrchestrator.java
+++ src/plays/TestOrchestrator.java
@@ -31,12 +31,30 @@ public class TestOrchestrator {
         orchestrator.play();
         scanner.close(); */
 
-        // Test entre joueurs et NegaMax player sur le Nim
+        // Test entre joueur et NegamaxPlayer sur le Nim (13 allumettes, 3 max)
         
-        Scanner scanner = new Scanner(System.in);
+       /*  Scanner scanner = new Scanner(System.in);
         Player player1 = new Human("Moi", scanner);
         Player player2 = new NegamaxPlayer();
         Nim game = new Nim(13, 3, player1, player2);
+        Orchestrator orchestrator = new Orchestrator(game);
+        orchestrator.play();
+        scanner.close(); */
+
+        // Test entre joueur et NegamaxPlayer (14 allumettes 3 max)
+       /*  Scanner scanner = new Scanner(System.in);
+        Player player1 = new NegamaxPlayer();
+        Player player2 = new Human("Moi", scanner);
+        Nim game = new Nim(14, 3, player1, player2);
+        Orchestrator orchestrator = new Orchestrator(game);
+        orchestrator.play();
+        scanner.close(); */
+
+        // Test entre joueur et NegamaxPlayer sur le TicTacToe
+        Scanner scanner = new Scanner(System.in);
+        Player player1 = new NegamaxPlayer();
+        Player player2 = new Human("Moi", scanner);
+        TicTacToe game = new TicTacToe(player1, player2);
         Orchestrator orchestrator = new Orchestrator(game);
         orchestrator.play();
         scanner.close();
