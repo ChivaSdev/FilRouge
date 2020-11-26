df4ddca77ce81693f541b43f68c06827716797b5
QuentinLireux <Sauron.gamers@gmail.com>
2020-11-26 15:04:40 +0100

    Ajout du TicTacToeWithHints.java

--- src/games/TicTacToeWithHints.java
+++ src/games/TicTacToeWithHints.java
@@ -1,7 +1,8 @@
-/* package games;
+package games;
 
-import java.util.ArrayList;
 import players.Player;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class TicTacToeWithHints extends TicTacToe {
 
@@ -9,7 +10,30 @@ public class TicTacToeWithHints extends TicTacToe {
         super(player1, player2);
     }
 
-    public ArrayList<Integer> hints() {
-        
+    public ArrayList<Integer> hints()
+    {
+      Player opponent;
+      ArrayList<Integer> listTempHints = super.validMoves();
+      ArrayList<Integer> listHints = new ArrayList<Integer>();
+
+      if(super.getCurrentPlayer() == super.player1)
+      {
+        opponent = super.player2;
+      }
+      else
+      {
+        opponent = super.player1;
+      }
+
+      for (Integer i : listTempHints)
+      {
+        super.gameTab[i/3][i%3] = opponent;
+        if(super.getWinner() == opponent)
+        {
+          listHints.add(i);
+        }
+        super.gameTab[i/3][i%3] = null;
+      }
+      return listHints;
     }
-} */
+}
